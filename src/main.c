553aa374ced881535bfee9dc91d32af2fcdf91d0
Nathan Piel <nthnpiel@gmail.com>
2022-08-13 17:17:51 -0400

    debug symbols are gud

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ const char HELP_MESSAGE[] =
     "\n"                                                                \
     "debug: OUTPUT = $(DEBUG)\n"                                        \
     "# Non production ready flags (as of 2021-09-01), https://github.com/google/sanitizers/issues/1324: -fsanitize=pointer-compare -fsanitize=pointer-subtract\n" \
-    "debug: FLAGS += -fsanitize=address -fsanitize=leak -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=bounds-strict -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow $(shell export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_invalid_pointer_pairs=2) -fanalyzer\n" \
+    "debug: FLAGS += -fsanitize=address -fsanitize=leak -fno-omit-frame-pointer -fsanitize=undefined -fsanitize=bounds-strict -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow $(shell export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:detect_invalid_pointer_pairs=2) -fanalyzer -g\n" \
     "debug: executable\n"                                               \
     "\n"
     
