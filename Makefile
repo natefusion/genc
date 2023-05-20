06df7b3912e88cdd363611b9fe46fe49e68bfc00
Nathan Piel <nthnpiel@gmail.com>
2023-05-19 20:51:14 -0400

    remove some flags

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ mkdir:
 
 SRC = $(wildcard src/*.c)
 CC = gcc
-FLAGS = -Wall -Werror -Wextra -Wpedantic -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2 -Wformat-security -Wnull-dereference -Wstack-protector -Wtrampolines -Walloca -Wvla -Warray-bounds=2 -Wimplicit-fallthrough=3 -Wtraditional-conversion -Wshift-overflow=2 -Wcast-qual -Wstringop-overflow=4 -Wconversion -Warith-conversion -Wlogical-op -Wduplicated-cond -Wduplicated-branches -Wformat-signedness -Wshadow -Wstrict-overflow=4 -Wundef -Wstrict-prototypes -Wswitch-default -Wswitch-enum -Wstack-usage=1000000 -Wcast-align=strict -D_FORTIFY_SOURCE=2 -fstack-protector-strong -fstack-clash-protection -fPIE -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-z,separate-code -pipe -O2
+FLAGS = -Wall -pipe -O2
 FLAGS += $(shell pkg-config --libs libgit2)
 
 debug: OUTPUT = $(DEBUG)
--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ const char HELP_MESSAGE[] =
 #define MAKEFILE_C                                                      \
     "SRC = $(wildcard src/*.c)\n"                                       \
     "CC = gcc\n"                                                        \
-    "FLAGS = -Wall -Werror -Wextra -Wpedantic -Wformat=2 -Wformat-overflow=2 -Wformat-truncation=2 -Wformat-security -Wnull-dereference -Wstack-protector -Wtrampolines -Walloca -Wvla -Warray-bounds=2 -Wimplicit-fallthrough=3 -Wtraditional-conversion -Wshift-overflow=2 -Wcast-qual -Wstringop-overflow=4 -Wconversion -Warith-conversion -Wlogical-op -Wduplicated-cond -Wduplicated-branches -Wformat-signedness -Wshadow -Wstrict-overflow=4 -Wundef -Wstrict-prototypes -Wswitch-default -Wswitch-enum -Wstack-usage=1000000 -Wcast-align=strict -D_FORTIFY_SOURCE=2 -fstack-protector-strong -fstack-clash-protection -fPIE -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -Wl,-z,separate-code -pipe -O2\n" \
+    "FLAGS = -Wall -pipe -O2"                                           \
     "\n"                                                                \
     "debug: OUTPUT = $(DEBUG)\n"                                        \
     "# Non production ready flags (as of 2021-09-01), https://github.com/google/sanitizers/issues/1324: -fsanitize=pointer-compare -fsanitize=pointer-subtract\n" \
